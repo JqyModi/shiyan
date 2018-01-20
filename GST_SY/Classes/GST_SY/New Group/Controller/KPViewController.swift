0a672835b995da78cb0b74fea5e1b21811a9a3ed
JqyModi <1294432350@qq.com>
2018-01-20 09:28:13 +0800

    修复因数据加载异步导致界面显示空白问题+新增TabBar

--- GST_SY/Classes/GST_SY/New Group/Controller/KPViewController.swift
+++ GST_SY/Classes/GST_SY/New Group/Controller/KPViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 import Tabman
 import Pageboy
+import SnapKit
 
 class KPViewController: TabmanViewController, PageboyViewControllerDataSource {
 
-//    var viewControllers: [UIViewController]?
-    private var viewControllers = [UIViewController]()
+    var viewControllers = [UIViewController]()
     
     //title是系统自带属性不需要重新定义
     var paperId: String?
@@ -29,31 +29,37 @@ class KPViewController: TabmanViewController, PageboyViewControllerDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.init(white: 0.96, alpha: 1.0)
-        self.dataSource = self
         
-        // bar customisation
-        bar.location = .top
-        //        bar.style = .custom(type: CustomTabmanBar.self) // uncomment to use CustomTabmanBar as style.
-//        bar.appearance = PresetAppearanceConfigs.forStyle(self.bar.style, currentAppearance: self.bar.appearance)
+        setupUI()
+        
+        self.dataSource = self
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor.init(white: 0.96, alpha: 1.0)
+        
+        view.addSubview(tabBar)
+        
+        //添加约束
+        tabBar.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalTo(view)
+            make.height.equalTo(50)
+        }
     }
     
     private func initializeViewControllers(papers: [Paper]) {
         var viewControllers = [UIViewController]()
         var barItems = [Item]()
-        
         for index in 0 ..< papers.count {
             let viewController = ExaminationViewController()
             viewController.paper = papers[index]
-//            viewController.index = index + 1
+            viewController.index = index + 1
             barItems.append(Item(title: "Page No. \(index + 1)"))
-//            let viewController = UIViewController()
-//            viewController.view.backgroundColor = UIColor.randomColor
-//            barItems.append(Item(title: "Page No. \(index + 1)"))
             viewControllers.append(viewController)
         }
-        
-        bar.items = barItems
+        //不添加页码显示
+//        bar.items = barItems
+//        bar.style = .scrollingButtonBar
         self.viewControllers = viewControllers
     }
     
@@ -61,37 +67,42 @@ class KPViewController: TabmanViewController, PageboyViewControllerDataSource {
         YMNetworkTool.shareNetworkTool.getPaper(paperId!){ [weak self](items) in
             self?.papers = items
             self?.initializeViewControllers(papers: (self?.papers)!)
-            print("papers.count  = \(self!.papers.count)")
-            if items.count>0 {
-                print("题目条数：\(items.count)")
-            }else{
-                print("暂无内容")
-//                Toast(text: "服务器数据异常，请稍后再试").show()
-            }
-            
+            //重新加载数据
+            self?.reloadPages()
         }
     }
-
     
+    //懒加载一个UITabBar
+    lazy var tabBar: UITabBar = {
+        let tabbar = UITabBar()
+        //设置Tabbar颜色
+        tabbar.backgroundColor = UIColor.white
+        let item1 = UITabBarItem(title: "上一题", image: UIImage(named: "pre"), tag: 0)
+        let item2 = UITabBarItem(title: "提交", image: UIImage(named: "submit"), tag: 1)
+        item2.isEnabled = false
+        let item3 = UITabBarItem(title: "下一题", image: UIImage(named: "next"), tag: 2)
+        let items = [item1,item2,item3]
+        tabbar.setItems(items, animated: true)
+//        tabbar.delegate = self
+        return tabbar
+    }()
+
+}
+
+extension KPViewController {
     func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
-        
-        return self.viewControllers.count ?? 0
-//        return self.papers.count
+        //一开始进来没有数据：异步加载数据：数据加载完成重新刷新数据
+        return viewControllers.count
     }
     
     func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
-//                let vc = ExaminationViewController()
-//                vc.paper = papers[index]
-//                return vc
-        
+//        let vc = ExaminationViewController()
+//        vc.paper = self.papers[index]
+//        return vc
         return self.viewControllers[index]
     }
     
     func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
         return nil
     }
-}
-
-extension KPViewController {
-
 }
